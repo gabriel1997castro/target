23069d2fb1828ac10f51fec271713dba0e4897e9
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-10-23 12:54:48 -0300

    Add gradient

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,12 @@
 import { Text, View, Button } from "react-native";
 import { router } from "expo-router";
 import { fontFamily } from "@/theme/fontFamily";
+import { HomeHeader } from "@/components/HomeHeader";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontFamily: fontFamily.bold, fontSize: 24 }}>
-        Expo router
-      </Text>
-
-      <Button title="New Target" onPress={() => router.navigate("/target")} />
-      <Button
-        title="Transaction"
-        onPress={() => router.navigate("/transaction/135")}
-      />
-      <Button
-        title="Progress"
-        onPress={() => router.navigate("/in-progress/13")}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   );
 }
