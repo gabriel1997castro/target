551a5cff4115972ac4a5bc7345e950002ce17bb4
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-10-23 12:46:43 -0300

    Add font and loading component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,21 @@
 import { colors } from "@/theme/colors";
 import { Stack } from "expo-router";
+import {
+  useFonts,
+  Inter_700Bold,
+  Inter_500Medium,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+import { Loading } from "@/components/Loading";
 
 export default function Layout() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+
+  if (!fontLoaded) return <Loading />;
   return (
     <Stack
       screenOptions={{
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
 import { Text, View, Button } from "react-native";
 import { router } from "expo-router";
+import { fontFamily } from "@/theme/fontFamily";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Expo router</Text>
+      <Text style={{ fontFamily: fontFamily.bold, fontSize: 24 }}>
+        Expo router
+      </Text>
 
       <Button title="New Target" onPress={() => router.navigate("/target")} />
       <Button
