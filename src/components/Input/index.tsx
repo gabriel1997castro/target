ff6372275c19459725916f8569f4575a8bbf08a2
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-10-25 23:10:10 -0300

    fix input type

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,7 +7,7 @@ type Props = TextInputProps & {
   label: string;
 };
 
-export function Input({ label, ...rest }) {
+export function Input({ label, ...rest }: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
