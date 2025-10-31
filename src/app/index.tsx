baa51e14a61c9012e405dbb4875274aa1ada5813
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-10-30 21:40:36 -0300

    Updates target list ordering by closest target

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -23,7 +23,7 @@ export default function Index() {
 
   async function fetchTargets(): Promise<TargetProps[]> {
     try {
-      const response = await targetDatabase.listBySavedValue();
+      const response = await targetDatabase.listByClosestTarget();
 
       return response.map((item) => ({
         id: String(item.id),
